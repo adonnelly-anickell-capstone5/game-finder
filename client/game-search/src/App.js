d417a6662ed1aa33dbf893b04d1d1775073fdb46
Alexandra Donnelly <adonnelly246@gmail.com>
2021-09-03 11:39:45 -0500

    init commit and set up

---
+++ client/game-search/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Form from './Form.js'
+
+export default function App(){
+    const [games, setGames] = useState([])
+    
+    //GET
+   
+
+    //POST
+  
+
+    //DELETE
+  
+
+    //PUT
+  
+
+    return(
+        <div>
+            <Form 
+                submit= {}
+                btnText ="Search"
+            />
+            <div>
+             
+                
+            </div>
+        </div>
+    )
+
+}
