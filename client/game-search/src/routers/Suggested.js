7bf3ebd9a6abdf8346d5cdc55b84511b08ef5d29
Alexandra Donnelly <adonnelly246@gmail.com>
2021-09-09 11:35:18 -0500

    took edit away for now may add back later

--- client/game-search/src/routers/Suggested.js
+++ client/game-search/src/routers/Suggested.js
@@ -3,7 +3,6 @@ import Search from "./Search.js"
 import Form from './Form.js'
 
 function Suggested(props){
-    const [edit, setEdit] = useState(false)
     const [favorite, setFavorite] = useState([]);
 
     const handleToggle = (id) => {
@@ -12,8 +11,6 @@ function Suggested(props){
 
         return(
             <div>
-                {/* {!edit ?   
-                <> */}
                     <Search/>
                     {/* searchTerm prop from Form */}
                     <h1>Results for {props.inputs.searchTerm}</h1> 
@@ -27,22 +24,7 @@ function Suggested(props){
                                     color={favorite.indexOf(index) > 0 ? 'red' : 'blue'}> </button>
                                 })}
                             
-                    
-                {/* </>
-                
-                :
-                <>
-                    <Search/>
-                    <button onClick= {props.getGames()}>SEARCH!</button>
-                    <h1>Results</h1>
-                    
-                            <ul><li>{props.games.map(game=>{game.title})}</li></ul>  
-                            <button onClick={props.handleToggle()}>Save Game</button>
-                            <button onClick={()=>setFavorite(prevToggle => !prevToggle)}>Toggle Off</button> 
-                            {/*  heart image-- full*/}
-                    {/* </>  
-                
-                } */} 
+                     
             </div> 
 
             
