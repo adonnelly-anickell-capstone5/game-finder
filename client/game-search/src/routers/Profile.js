5f33a05402d772aae70eeb1bb4a69577ae1e2231
Alexandra Donnelly <adonnelly246@gmail.com>
2021-10-08 14:06:20 -0500

    fixed react errors on my end

--- client/game-search/src/routers/Profile.js
+++ client/game-search/src/routers/Profile.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import NewGame from "./NewGame"
-
+import App from "../App.js"
 
 export default function Profile(props){
     const faveGames = props.faveGames
@@ -15,7 +15,8 @@ export default function Profile(props){
                 <h3>Players: {game.players}</h3> 
                 <h3>Category: {game.category}</h3>
                 <h3>Console: {game.gameConsole}</h3>    
-                <h3>Rating: {game.rating}</h3>          
+                <h3>Rating: {game.rating}</h3> 
+                <button onClick={()=>props.deleteFave(game._id)}>Delete From List</button>         
            </div>
            
 
@@ -35,12 +36,12 @@ export default function Profile(props){
 }
 
 
- {/* <h1>Your Favorite Games List</h1>
-           {faveGames.map((item)=>( 
-                        <li key={item._id}>
-                            <h2>{item.title} </h2>
-                            <p>Players: {item.players}</p>
-                            <p>Category: {item.category}</p>
-                            <p>Console: {item.gameConsole}</p>
-                            <button onClick={()=>props.deleteFave(item._id)}>Delete From List</button>
-                        </li>)) } */}
+//   <h1>Your Favorite Games List</h1>
+//            {faveGames.map((item)=>( 
+//                         <li key={item._id}>
+//                             <h2>{item.title} </h2>
+//                             <p>Players: {item.players}</p>
+//                             <p>Category: {item.category}</p>
+//                             <p>Console: {item.gameConsole}</p>
+//                             <button onClick={()=>props.deleteFave(item._id)}>Delete From List</button>
+//                         </li>)) } 
